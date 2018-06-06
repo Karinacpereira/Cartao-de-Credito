1b5a47d612ce8b9157bbf293211d677d920514d3
Karinacpereira <39533739+Karinacpereira@users.noreply.github.com>
2018-06-06 01:24:40 -0300

    Primeira tentativa - html pronto, js com esboço

---
+++ app.js
@@ -0,0 +1,12 @@
+var arrayCard = [];
+var creditCard = parseInt(prompt('Insira aqui o número do seu cartão de crédito.'));
+  for(var i = 0; i < creditCard.length; i++){
+    arrayCard.push(creditCard[i]);
+  isValidCard(creditCard);
+  }
+
+  var arrayInvert = arrayCard.reverse();
+
+
+function isValidCard(cardNumber){
+}
