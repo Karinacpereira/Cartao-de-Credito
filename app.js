bdbd303af1e5bc7b41e15b81a4613ebd1e800fb8
Karinacpereira <39533739+Karinacpereira@users.noreply.github.com>
2018-06-07 01:22:31 -0300

    Produto Final
    Produto final com js completo - teste feito com meus cartões, rodando ok!

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-var arrayCard = [];
-var creditCard = parseInt(prompt('Insira aqui o número do seu cartão de crédito.'));
-  for(var i = 0; i < creditCard.length; i++){
-    arrayCard.push(creditCard[i]);
-  isValidCard(creditCard);
-  }
-
-  var arrayInvert = arrayCard.reverse();
-
+do {
+  var cardNumber = prompt('Insira aqui o número do seu cartão de crédito.');
+  isValidCard(cardNumber);
+} while(cardNumber.length < 1);
 
 function isValidCard(cardNumber){
+  var arrayReverse = [];
+  var arrayNumber = [];
+  var sum = 0;
+
+  for (var i = cardNumber.length - 1; i >= 0; i--){
+    arrayReverse.push(cardNumber[i]);
+  }
+
+  for (var j = 0; j < arrayReverse.length; j++){
+    if (j % 2 === 1){
+      var multiplication = arrayReverse[j] * 2;
+      if (multiplication >= 10){
+        var subtraction = multiplication - 9;
+        arrayNumber.push(subtraction);
+      } else {
+        arrayNumber.push(multiplication);
+      }
+    } else {
+      arrayNumber.push(parseInt(arrayReverse[j]));
+    }
+  }
+
+  for (var k = 0; k < arrayNumber.length; k++){
+    sum += arrayNumber[k];
+  }
+
+  if (sum % 10 === 0){
+    return document.write('Ok, número de cartão válido!');
+  } else {
+    return document.write('Número de cartão inválido, por favor, tente novamente.')
+  }
 }
